02b2326dccb69b23e1957d36f53c5c3b568f7721
brunoBaziret <bruno.baziret@gmail.com>
2022-01-06 21:35:49 +0100

    Add additional messages to hero service https://angular.io/tutorial/toh-pt4 Add services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit} from '@angular/core';
+
 import {Hero} from "../hero";
 // import {HEROES} from "../mock-heroes";
 import {HeroService} from "../hero.service";
+import {MessageService} from "../message.service";
 
 @Component({
   selector: 'app-heroes',
@@ -16,23 +18,24 @@ export class HeroesComponent implements OnInit {
 
   selectedHero?: Hero;
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+  heroes: Hero[] = [];
 
   // heroes = HEROES;
 
-  heroes: Hero[] = [];
-
-  constructor(private heroService: HeroService) {
-  }
-
-  getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
+  constructor(private heroService: HeroService, private messageService: MessageService) {
   }
 
   ngOnInit(): void {
     this.getHeroes();
   }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id =${hero.id}`)
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
 }
